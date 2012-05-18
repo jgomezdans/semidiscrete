3addfdfca6e6c88ad268787bef04d31a08870fed
Jose Gomez-Dans <jgomezdans@gmail.com>
2012-05-18 18:30:44 +0100

    setup file compiles, still need to add methods to __init__py

--- README.rst
+++ README.rst
@@ -2,3 +2,21 @@ semidiscrete
 ============
 
 Python bindings for the semidiscrete RT model of Gobron et al.
+
+Note this version includes adjoint code developed by P Lewis and J Gómez-Dans.
+The original semidiscrete code can be found `here <http://fapar.jrc.ec.europa.eu/WWW/Data/Pages/FAPAR_Software/FAPAR_Software_RTModels_1-2Discrete.php>`_
+
+The copyright for the original code is
+
+    Copyrights
+    © Nadine Gobron, Bernard Pinty, and Michel M. Verstraete 1997 The copyrights for these 8 files, including the computer codes, remain with their authors, Drs. Nadine Gobron, Bernard Pinty and Michel M. Verstraete.
+
+    Academic users
+    You are authorized to use this code for your research and teaching, but you must acknowledge use of this routine explicitly and refer to the paper above in any publication or work for which you used these codes. 
+    You may distribute, free of charge, the unmodified version of these codes to colleagues involved in similar activities, provided you include all the in-line documentation. They, in turn, must agree with and abide by the same rules. 
+    You may not sell this code to anybody, and you may not distribute it to commercial interests under any circumstances.
+
+    Commercial and other users
+    Use of this code (NADIM) in commercial applications is strictly forbidden without the written approval of the authors. Even with such an authorization to use the code, you may not distribute or sell it to any other commercial or business partners under any circumstances.
+
+    
--- setup.py
+++ setup.py
@@ -1,26 +1,40 @@
-from setuptools import setup, find_packages
+from numpy.distutils.core import setup
+
 import sys, os
 
 version = '1.0'
 
-setup(name='semidiscrete',
-      version=version,
-      description="Python bindings to the semidiscrete RT code",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='',
-      author='J Gomez-Dans',
-      author_email='j.gomez-dans@ucl.ac.uk',
-      url='',
-      license='',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          # -*- Extra requirements: -*-
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+def configuration ( parent_package='', top_path=None ):
+    from numpy.distutils.misc_util import Configuration
+    config = Configuration('semidiscrete',parent_package,top_path, )
+    
+    config.add_extension ('rtmodel_ad_trans2', 
+    sources = ['semidiscrete/rtmodel_ad_trans2.pyf', \
+        "semidiscrete/dataSpec_P5.f90",  "semidiscrete/leaf.f90", \
+        "semidiscrete/msnadimbrf_ad.f90", "semidiscrete/nadimmod_ad.f90", \
+        "semidiscrete/rtmodel_ad_trans.f90", "semidiscrete/soil.f90", \
+        "semidiscrete/tav_abs.f90", "semidiscrete/nadimbrf_ad.f",  \
+        "semidiscrete/nadimtools_ad.f" ] )
+    return config
+
+if __name__ == "__main__":    
+    # This is a hack, as F77/F90 flags can be passed in the config object
+    # but only on recent versions of numpy/f2py
+    sys.argv.extend ( ["config_fc", "--fcompiler=gnu95", 
+            "--f90exec=/usr/bin/gfortran44", 
+            "--f77flags='-ffixed-form -ffixed-line-length-none'" ])
+
+    name         = "semidiscrete"  # name of the generated python extension (.so)
+    description  = "semidiscrete python wrappers"
+    author       = "J Gomez-Dans/NCEO & University College London"
+    author_email = "j.gomez-dans@ucl.ac.uk"
+    url="http://fapar.jrc.ec.europa.eu/WWW/Data/Pages/FAPAR_Software/FAPAR_Software_RTModels_1-2Discrete.php"
+    
+    setup( name=name,\
+        description=description, \
+        author=author, \
+        author_email = author_email, \
+        url=url,
+        configuration = configuration, version="1.0",\
+        packages=["semidiscrete"])
+    
